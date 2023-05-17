1311cfaf441546a35fe0275a03ed8534e217e445
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-05-16 18:06:54 -0700

    data.c updated

--- data.c
+++ data.c
@@ -1,140 +1,140 @@
-#include"shell.h"
+#include "shell.h"
 
 /**
-* _erratoi - converts a string to an integer
-* @s: the string to be converted
-*
-* Return: the converted integer if successful, -1 on error
-*/
-int_erratoi(char *s)
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the converted integer if successful, -1 on error
+ */
+int _erratoi(char *s)
 {
-int i = 0;
-unsigned long int result = 0;
+	int i = 0;
+	unsigned long int result = 0;
 
-if (*s == '+')
-s++;  /* TODO: why does this make main return 255? */
-for (i = 0;  s[i] != '\0'; i++)
-{
-if (s[i] >= '0' && s[i] <= '9')
-{
-result *= 10;
-result += (s[i] - '0');
-if (result > INT_MAX)
-return (-1);
-}
-else
-return (-1);
-}
-return (result);
-}
-
-/**
-* print_error - prints an error message
-* @info:  pointer to the info struct
-* @estr: string containing the specified error type
-*
-* Return: nothing
-*/
-voidprint_error(data *info, char *estr)
-{
-_eputs(info->fname);
-_eputs(": ");
-print_d(info->line_count, STDERR_FILENO);
-_eputs(": ");
-_eputs(info->argv[0]);
-_eputs(": ");
-_eputs(estr);
+	if (*s == '+')
+		s++;
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
 }
 
 /**
-* print_d - function prints a decimal (integer) number (base 10)
-* @input: the integer to print
-* @fd: the file descriptor to write to
-*
-* Return: number of characters printed
-*/
-intprint_d(int input, int fd)
+ * print_error - prints an error message
+ * @info:  pointer to the info struct
+ * @estr: string containing the specified error type
+ *
+ * Return: nothing
+ */
+void print_error(data *info, char *estr)
 {
-int (*__putchar)(char) = _putchar;
-int i, count = 0;
-unsigned int _abs_, current;
-
-if (fd == STDERR_FILENO)
-__putchar = _eputchar;
-if (input < 0)
-{
-_abs_ = -input;
-__putchar('-');
-count++;
-}
-else
-_abs_ = input;
-current = _abs_;
-for (i = 1000000000; i > 1; i /= 10)
-{
-if (_abs_ / i)
-{
-__putchar('0' + current / i);
-count++;
-}
-current %= i;
-}
-__putchar('0' + current);
-count++;
-
-return (count);
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
 }
 
 /**
-* convert_number - a function that converts a given integer to string
-* @num: The number to be converted
-* @base: The base to convert the number to
-* @flags: Additional flags to specify conversion settings
-*
-* Return: The resulting string
-*/
-char*convert_number(long int num, int base, int flags)
+ * print_d - function prints a decimal (integer) number (base 10)
+ * @input: the integer to print
+ * @fd: the file descriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_d(int input, int fd)
 {
-static char *array;
-static char buffer[50];
-char sign = 0;
-char *ptr;
-unsigned long n = num;
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
 
-if (!(flags & CONVERT_UNSIGNED) && num < 0)
-{
-n = -num;
-sign = '-';
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
 
-}
-array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-tr = &buffer[49];
-*ptr = '\0';
-
-do	{
-*--ptr = array[n % base];
-n /= base;
-} while (n != 0);
-
-if (sign)
-*--ptr = sign;
-return (ptr);
+	return (count);
 }
 
 /**
-* remove_comments - function replaces first instance of '#' with '\0'
-* @buf: address of the string to modify
-*
-* Return: Always 0
-*/
-voidremove_comments(char *buf)
+ * convert_number - a function that converts a given integer to string
+ * @num: The number to be converted
+ * @base: The base to convert the number to
+ * @flags: Additional flags to specify conversion settings
+ *
+ * Return: The resulting string
+ */
+char *convert_number(long int num, int base, int flags)
 {
-int i;
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
 
-for (i = 0; buf[i] != '\0'; i++)
-if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
-{
-buf[i] = '\0';
-break;
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
 }
+
+/**
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0
+ */
+void remove_comments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
 }
